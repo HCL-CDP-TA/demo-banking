f6286bda7738fe9724945046cff1225fafba9395
Simon Pallister <simon@pallisters.net>
2025-07-25 14:45:07 +1000

    status popover

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { SiteProvider } from "@/lib/SiteContext"
 import CdpProvider from "@/components/CdpProvider"
+import StatusPopover from "@/components/StatusPopover"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <SiteProvider>
           <CdpProvider>{children}</CdpProvider>
+          <StatusPopover />
         </SiteProvider>
       </body>
     </html>
